d1884319230f8b29e38c1f43af46fc09584bdf40
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-26 07:51:51 +0100

    Updated the button tab ui

--- src/components/HomeTabButtons.vue
+++ src/components/HomeTabButtons.vue
@@ -1,6 +1,6 @@
 <template>
-  <span role="group" class="flex bg-gray-600 bg-opacity-50 rounded-md">
-    <button-tab class="bg-white text-teal">Buy</button-tab>
+  <span role="group" class="flex bg-gray-600 bg-opacity-50 rounded-lg">
+    <button-tab>Buy</button-tab>
     <button-tab>Rent</button-tab>
     <button-tab>Sold</button-tab>
   </span> 
