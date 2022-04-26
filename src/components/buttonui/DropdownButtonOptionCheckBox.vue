bf391dc90a739dc9f403be6a34b92be26f7c27d1
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-04-26 16:16:52 +0100

    Attribute inheritance deactivated and implemented and v-model applied

--- src/components/buttonui/DropdownButtonOptionCheckBox.vue
+++ src/components/buttonui/DropdownButtonOptionCheckBox.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="items-start">
-    <!-- <label for="" class="inline-flex items-center"></label> -->
-    <div class="flex items-center h-6">
-      <input 
-        type="checkbox" 
-        class="h-4 w-4 text-teal border border-gray-200 rounded" 
-        name="" 
-        value=""
-        true-value="yes" 
-        false-value="no"/>
-      <div class="ml-2"><slot></slot></div>
-    </div>
+  <div>
+    <label :for="forAttr">
+      <div class="flex flex-row items-center h-6 cursor-pointer">
+        <input
+          v-model="checkedHomeType" 
+          type="checkbox"
+          v-bind="$attrs" 
+          class="inline-block h-4 w-6 text-teal border border-gray-200 rounded" 
+          true-value="yes" 
+          false-value="no">
+        <div class="ml-2"><slot></slot></div>
+      </div>
+    </label>
   </div>
 </template>
+<script>
+import { ref } from "vue";
+export default {
+  inheritAttrs: false,
+  setup(props, ctx) {
+    // fallthrough attributes are exposed as ctx.attrs
+    const forAttr = ref(ctx.attrs.id);
+    const checkedHomeType = ref([]);
+    return {
+      forAttr,
+      checkedHomeType
+    }
+  }
+}
+</script>
