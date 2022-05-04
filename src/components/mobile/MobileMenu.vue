c3c8fab5f56d7ba347b0ca21ff5743e3bcd66016
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-04 14:35:05 +0100

    Mobile responsive implementation

---
+++ src/components/mobile/MobileMenu.vue
@@ -0,0 +1,11 @@
+<template>
+  <div 
+    class="md:w-1/4 w-2/5 fixed -top-0 -right-0 h-screen bg-gray-700 text-white">
+
+  </div>
+</template>
+<script>
+export default {
+  name: "MobileMenu",
+}
+</script>
