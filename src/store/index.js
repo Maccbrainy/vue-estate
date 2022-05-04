eff444bf155b97890f6648be607691cd2fa2972c
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-04 11:46:05 +0100

    Updated the store and setting initial state

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,39 @@
 import { createStore } from "vuex";
+
 export default createStore({
   state: {
+    listingBranchByAgent: "active",
     searchedData: {},
     successfulSearchHistory: [{}],
+    allPropertyListings: {},
+    propertyListingsByAgent: {},
+    propertyListingsByNoneAgent: {},
+    activeListing: {},
   },
   getters: {
+    getListingBranchByAgent(state){
+      return state.listingBranchByAgent ? state.listingBranchByAgent : null;
+    },
+    //Consumed in searchResultContentLayout component
     getSearchedData(state){
-      if (!state.searchedData){
-        return;
-      } else{
-        return state.searchedData;
-      }
+      return state.searchedData ? state.searchedData : null; 
+    },
+    //Consumed in searchResultContentLayout component
+    getAllPropertyListings(state){
+      return state.allPropertyListings ? state.allPropertyListings : null;
+    },
+    getPropertyListingsByAgent(state){
+      return state.propertyListingsByAgent 
+        ? state.propertyListingsByAgent : null;
+    },
+    getPropertyListingsByNoneAgent(state){
+      return state.propertyListingsByNoneAgent 
+        ? state.propertyListingsByNoneAgent : null;
+    },
+    getActiveListing(state){
+      return state.activeListing ? state.activeListing : null;
     }
+
   },
   mutations: {
     setSearchedData(state, searchedPayLoad){
@@ -19,7 +41,27 @@ export default createStore({
     },
     setSuccessfulSearchHistory(state, searchedPayLoad){
       state.successfulSearchHistory.unshift(searchedPayLoad);
-    }
+    },
+    //Invoked from https request in the searchResultContentLayout component
+    setAllPropertyListings(state, propertyPayLoad){
+      state.allPropertyListings = propertyPayLoad;
+    },
+    //Invoked from computedAgentAndNoneAgent
+    setListingsByAgent(state, propertyPayLoad){
+      state.propertyListingsByAgent = propertyPayLoad;
+    },
+    //Invoked from computedAgentAndNoneAgent component
+    setListingsByNoneAgent(state, propertyPayLoad){
+      state.propertyListingsByNoneAgent = propertyPayLoad;
+    },
+    //Invoked from filter button Toggle Active component
+    setListingBranchByAgent(state, propertyPayLoad){
+      state.listingBranchByAgent = propertyPayLoad;
+    },
+    //Invoked from computedAgentAndNoneAgent component
+    setActiveListing(state, propertyPayLoad){
+      state.activeListing = propertyPayLoad;
+    },
   },
 
   actions: {},
