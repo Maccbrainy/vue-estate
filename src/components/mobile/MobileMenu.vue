38ec3c77b6b980348d16d9296aa1c3a753faa085
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-04 20:27:52 +0100

    Code refactor

--- src/components/mobile/MobileMenu.vue
+++ src/components/mobile/MobileMenu.vue
@@ -1,11 +1,59 @@
 <template>
-  <div 
-    class="md:w-1/4 w-2/5 fixed -top-0 -right-0 h-screen bg-gray-700 text-white">
-
-  </div>
+  <mobile-menu-container>
+    <mobile-menu-list>
+      <mobile-menu-button-item>Sign up or Login</mobile-menu-button-item>
+      <mobile-menu-drop-down menuTitle="Buy">
+        <mobile-menu-drop-down-item>Homes for Sale</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Open Houses</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>New Homes</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Recently Sold</mobile-menu-drop-down-item>
+      </mobile-menu-drop-down>
+      <mobile-menu-drop-down menuTitle="Rent">
+        <mobile-menu-drop-down-item>Homes for Rent</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Open Rent</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>New Homes for Rent</mobile-menu-drop-down-item>
+      </mobile-menu-drop-down>
+      <mobile-menu-drop-down menuTitle="Mortgage">
+        <mobile-menu-drop-down-item>Mortgage Overview</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Get Pre-Qualified</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Mortgage Rates</mobile-menu-drop-down-item>
+      </mobile-menu-drop-down>
+      <mobile-menu-drop-down menuTitle="Local Info">
+        <mobile-menu-drop-down-item>All vue-estate neighborhood guide</mobile-menu-drop-down-item>
+      </mobile-menu-drop-down>
+      <mobile-menu-drop-down menuTitle="Additional Resources">
+        <mobile-menu-drop-down-item>Vue Estate app blog</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Get leads by Zip code</mobile-menu-drop-down-item>
+        <mobile-menu-drop-down-item>Help center</mobile-menu-drop-down-item>
+      </mobile-menu-drop-down>
+    </mobile-menu-list>
+  </mobile-menu-container>
 </template>
 <script>
+import { ref } from "vue";
+import {
+  MobileMenuContainer,
+  MobileMenuList,
+  MobileMenuButtonItem,
+  MobileMenuDropDown,
+  // MobileMenuDropDownList, 
+  MobileMenuDropDownItem 
+} from "@/components/mobile/index.js";
 export default {
   name: "MobileMenu",
+  components: {
+    MobileMenuContainer,
+    MobileMenuList,
+    MobileMenuButtonItem,
+    MobileMenuDropDown,
+    // MobileMenuDropDownList,
+    MobileMenuDropDownItem 
+  },
+  setup() {
+    const openMenu = ref(false);
+    return{
+      openMenu,
+    }
+  }
 }
 </script>
