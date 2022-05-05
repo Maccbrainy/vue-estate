11d3e80aecd09b98d6909740b20587d9eea35497
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-05 19:34:04 +0100

    Price range filter and commit to store

--- src/components/FilterButtonRange.vue
+++ src/components/FilterButtonRange.vue
@@ -1,32 +1,30 @@
 <template>
-  <dropdown-button buttonTitle="Any Price" menuTitle="Price Range">
+  <dropdown-button :buttonTitle="priceIndicator" menuTitle="Price Range">
     <div class="flex">
       <dropdown-button-select-box>
-        <dropdown-button-select>
-          <dropdown-button-select-option>No Min</dropdown-button-select-option>
-          <dropdown-button-select-option value="a">
-            A
+        <dropdown-button-select v-model="selectedMin">
+          <dropdown-button-select-option value>
+            No Min
           </dropdown-button-select-option>
-          <dropdown-button-select-option value="b">
-            B
-          </dropdown-button-select-option>
-          <dropdown-button-select-option value="c">
-            C
+          <dropdown-button-select-option 
+            v-for="priceRange in priceRanges"
+            v-bind:key="priceRange.index" 
+            v-bind:value="priceRange">
+            ${{ priceRange }}
           </dropdown-button-select-option>
         </dropdown-button-select>
       </dropdown-button-select-box>
       <button-separator></button-separator>
       <dropdown-button-select-box>
-        <dropdown-button-select>
-          <dropdown-button-select-option>No Max</dropdown-button-select-option>
-          <dropdown-button-select-option value="a">
-            A
+        <dropdown-button-select v-model="selectedMax"> 
+          <dropdown-button-select-option value>
+            No Max
           </dropdown-button-select-option>
-          <dropdown-button-select-option value="b">
-            B
-          </dropdown-button-select-option>
-          <dropdown-button-select-option value="c">
-            C
+          <dropdown-button-select-option 
+            v-for="priceRange in priceRanges"
+            v-bind:key="priceRange.index" 
+            v-bind:value="priceRange">
+            ${{ priceRange }}
           </dropdown-button-select-option>
         </dropdown-button-select>
       </dropdown-button-select-box>
@@ -41,6 +39,8 @@ import {
   DropdownButtonSelectBox, 
   DropdownButtonSelectOption, 
 } from "@/components/buttonui/index";
+import { ref, watchEffect, computed } from "vue";
+import { useStore } from "vuex";
 export default {
   name: "FilterButtonRange",
   components: {
@@ -49,6 +49,41 @@ export default {
     DropdownButtonSelect,
     DropdownButtonSelectBox,
     DropdownButtonSelectOption,
+  },
+  setup() {
+    const priceRanges = ref([ 
+      10000, 20000, 30000, 40000, 600000, 700000, 800000, 900000 
+    ]);
+    const store = useStore();
+    const selectedMin = ref("");
+    const selectedMax = ref("");
+
+    const priceIndicator = computed(() =>{
+      return !selectedMin.value && !selectedMax.value 
+        ? "Any Price" 
+        : `${selectedMin.value} - ${selectedMax.value}`
+    });
+
+    // console.log("Min:", selectedMin.value);
+    // console.log("Max:", selectedMax.value);
+
+    watchEffect(() => {
+      let minRange = selectedMin.value;
+      let maxRange = selectedMax.value;
+
+      store.commit("setMinPriceRange", minRange);
+      store.commit("setMaxPriceRange", maxRange);
+      // console.log("minRange price value:", minRange); 
+      // console.log("maxRange price value:", maxRange); 
+    });
+
+    return {
+      priceRanges,
+      selectedMin,
+      selectedMax,
+      priceIndicator
+
+    }
   }
 }
 </script>
