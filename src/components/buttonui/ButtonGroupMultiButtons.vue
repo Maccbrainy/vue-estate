74c90e2dba44826f87049efaa84f7533d91e64b5
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-09 11:02:54 +0100

    MultipleButtons reusable component

---
+++ src/components/buttonui/ButtonGroupMultiButtons.vue
@@ -0,0 +1,29 @@
+<template>
+  <button-group>
+    <button-singular 
+      v-for="option in options" 
+      v-bind:key="option.index"
+      v-on:click="$emit('getOptionId', $event)"
+      v-bind:id="option"
+      v-bind:class="{
+        'rounded-l-lg': option === options[0] ? true : false,
+        'border-r rounded-r-lg': 
+          option === options[options.length - 1] ? true : false,
+      }">
+      {{ option }}+
+    </button-singular>
+  </button-group>
+</template>
+<script>
+import { ButtonSingular, ButtonGroup } from "@/components/buttonui/index";
+export default ({
+  name: "ButtonGroupMultiButtons",
+  components: {
+    ButtonSingular,
+    ButtonGroup,
+  },
+  props: {
+    options: Array
+  }
+})
+</script>
