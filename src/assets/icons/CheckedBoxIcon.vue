42da76d81ea9d2954f9a7aa70f50dd4fcb6be302
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-09 08:51:59 +0100

    Custom checkbox icons added

---
+++ src/assets/icons/CheckedBoxIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    class="text-teal rounded shadow-xs" 
+    xmlns="http://www.w3.org/2000/svg" 
+    width="16" 
+    height="16"
+    fill="currentColor" 
+    viewBox="0 0 24 24">
+    <path 
+      d="M19 0h-14c-2.762 0-5 2.239-5 5v14c0 2.761 2.238 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-8.959 17l-4.5-4.319 1.395-1.435 3.08 2.937 7.021-7.183 1.422 1.409-8.418 8.591z"/>
+  </svg>
+</template>
+<style scoped>
+.shadow-xs {
+  box-shadow: 0 2px 10px -1px rgb(0 120 137);
+}
+</style>
