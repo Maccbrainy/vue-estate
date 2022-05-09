a002034abd560257db79708188162b7d5c171153
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-09 11:04:52 +0100

    Dynamic button generation using ButtonGroupMultiButtons reusable component

--- src/components/FilterButtonGroup.vue
+++ src/components/FilterButtonGroup.vue
@@ -1,29 +1,36 @@
 <template>
   <dropdown-button buttonTitle="All Beds" menuTitle="Bedrooms">
-    <button-group>
-      <button-singular 
-        class="rounded-l-md text-teal-lighter bg-teal hover:bg-teal">
-        Studio+
-      </button-singular>
-      <button-singular>1+</button-singular>
-      <button-singular>2+</button-singular>
-      <button-singular>3+</button-singular>
-      <button-singular class="border-r rounded-r-md">4+</button-singular>
-    </button-group>
+    <button-group-multi-buttons 
+      v-bind:options="bedOptions"
+      v-on:getOptionId="numberOfBed">
+    </button-group-multi-buttons>
   </dropdown-button>
 </template>
 <script>
+import { reactive } from "vue";
 import { 
-  ButtonSingular, 
-  ButtonGroup, 
-  DropdownButton 
+  DropdownButton,
+  ButtonGroupMultiButtons 
 } from "@/components/buttonui/index";
+import { useStore } from "vuex";
 export default ({
   name: "FilterButtonGroup",
   components: {
-    ButtonSingular,
-    ButtonGroup,
     DropdownButton,
+    ButtonGroupMultiButtons
+  },
+  setup() {
+    const store = useStore();
+    const bedOptions = reactive(["Studio", 1, 2, 3, 4]);
+
+    function numberOfBed(e){
+      console.log("You clicked me!", e.target.id);
+      store.commit("setNumberOfBedRoom", e.target.id);
+    }
+    return {
+      numberOfBed,
+      bedOptions,
+    }
   }
 })
 </script>
