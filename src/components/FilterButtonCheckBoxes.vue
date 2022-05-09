df709ec913ab5d010237bf9ae2999dc823eb5f0b
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-09 09:24:16 +0100

    Dynamic checkboxes/reusable component

--- src/components/FilterButtonCheckBoxes.vue
+++ src/components/FilterButtonCheckBoxes.vue
@@ -1,57 +1,41 @@
 <template>
   <dropdown-button buttonTitle="All Home Types" menuTitle="Home Type">
-    <dropdown-button-options>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box id="single" value="single">
-          Single
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box 
-          id="multi_family" 
-          value="multi_family">
-          Multi-family
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box id="town" value="town">
-          Town
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box id="land" value="land">
-          Land
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box 
-          id="mobile_manufactured" 
-          value="mobile">
-          Mobile/Manufactured
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-      <dropdown-button-option>
-        <dropdown-button-option-check-box id="others" value="others">
-          Others
-        </dropdown-button-option-check-box>
-      </dropdown-button-option>
-    </dropdown-button-options>
+    <dropdown-button-multi-check-boxes 
+      v-model:value="checkedHomeType" 
+      v-bind:propertyOptions="homeTypes">
+    </dropdown-button-multi-check-boxes>
+    <div>{{ checkedHomeType }}</div>
   </dropdown-button>
 </template>
 <script>
+import { ref } from "vue";
+// import { useStore } from "vuex";
 import { 
   DropdownButton, 
-  DropdownButtonOptions, 
-  DropdownButtonOption,
-  DropdownButtonOptionCheckBox, 
+  DropdownButtonMultiCheckBoxes 
 } from "@/components/buttonui/index";
 export default ({
   name: "FilterButtonCheckBoxes",
   components: {
     DropdownButton,
-    DropdownButtonOptions,
-    DropdownButtonOption,
-    DropdownButtonOptionCheckBox,
+    DropdownButtonMultiCheckBoxes
+  },
+  setup() {
+    // const store = useStore();
+    let checkedHomeType = ref([]);
+    let homeTypes = ref([
+      "Single", 
+      "Multi-Family", 
+      "Condo", 
+      "Land", 
+      "Mobile/Manufactured",
+      "Others"
+    ]);
+
+    return {
+      homeTypes,
+      checkedHomeType,
+    }
   }
 })
 </script>
