5461c9cf70b8b9bf491c60410cb92ceba0a8a94f
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-14 09:48:58 +0100

    Composable refactoring

---
+++ src/composables/index.js
@@ -0,0 +1,9 @@
+export { default as searchInfoQuerySanitizer } from "@/composables/searchInfoQuerySanitizer";
+export { default as buildRouterParamsUrl } from "@/composables/buildRouterParamsUrl";
+export { default as clientLocalStorage } from "@/composables/clientLocalStorage";
+export { default as searchResultTitle } from "@/composables/searchResultTitle";
+export { default as checkPresenceOfHyphenAndRemove } from "@/composables/checkPresenceOfHyphenAndRemove";
+export { default as checkWhiteSpacesAndReplaceWithHypen } from "@/composables/checkWhiteSpacesAndReplaceWithHypen";
+
+export { default as computedHomeResourcesSearched } from "@/composables/computedHomeResourcesSearched";
+export { default as computedPropertyByAgentAndNonAgent } from "@/composables/computedPropertyByAgentAndNonAgent";
