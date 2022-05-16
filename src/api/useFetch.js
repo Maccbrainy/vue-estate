2d12d3cc3d0e90855c8f9133d1080552ae4805cd
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-16 09:35:22 +0100

    API hook for fetching property details

---
+++ src/api/useFetch.js
@@ -0,0 +1,39 @@
+import { ref } from "vue";
+import { useStore } from "vuex";
+import axios from "axios";
+
+const url = "https://realty-in-us.p.rapidapi.com/properties/detail"
+
+export function useFetch(){
+  const store = useStore();
+  const propertyDetail = ref(null);
+  const error = ref(null);
+  const getPropertyDetails = async (propertyId) => {
+    store.commit("setIsLoading", true);
+    try {
+      propertyDetail.value = await axios.get(url, {
+        params: {
+          property_id: `${propertyId}`
+        },
+        headers: {
+          "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+          "X-RapidAPI-Key": `${process.env.VUE_APP_RAPID_API_KEY}`
+        }
+      });
+      const { 
+        data: { listing }
+      } = propertyDetail.value;
+      console.log("Detailed Properties fetched:", listing);
+      return listing;
+    } catch (err) {
+      console.error(err);
+      error.value = true;
+    } finally {
+      store.commit("setIsLoading", false);
+      console.log("Fetching is completed")
+    }
+  }
+  return {
+    getPropertyDetails, 
+  }
+}
