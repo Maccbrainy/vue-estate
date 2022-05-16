3109237c706bb40e475ef2f976606d5e50931ca3
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-16 09:47:11 +0100

    Updated to consume api data

--- src/components/SearchResultItemCard.vue
+++ src/components/SearchResultItemCard.vue
@@ -13,38 +13,42 @@
       2xl:w-4/12
       3xl:w-3/12">
     <div>
-      <div v-bind:id="home.name">
+      <div v-bind:id="home.property_id">
         <router-link 
           :to="{
             name: 'HomeDetail', 
             params: { 
-              slug: home.state,
-              city: homeCity,
-              address: homeStreet,
+              slug: home.address.state_code,
+              city: home.address.city,
+              address: home.address.line,
+              propertyId: home.property_id,
+              postalCode: home.address.postal_code,
             },
           }" 
           target="_blank">
           <widget-image-slider></widget-image-slider>
           <div class="block">
-            <widget-price-detail v-bind:selling-price="home.id">
+            <widget-price-detail v-bind:selling-price="home.price">
             </widget-price-detail>
             <widget-utility-summary 
-              v-bind:bedroom="home.no_bedroom" 
-              v-bind:bathroom="home.no_bathroom" 
-              v-bind:squarefoot="home.no_sqft">
+              v-bind:bedroom="home.beds" 
+              v-bind:bathroom="home.baths" 
+              v-bind:squarefoot="home.baths">
             </widget-utility-summary>
             <widget-location-detail 
-              v-bind:street="home.street"
-              v-bind:city="home.city"
-              v-bind:state="home.state"
-              v-bind:postal-code="home.postal_code">
+              v-bind:street="home.address.line"
+              v-bind:city="home.address.city"
+              v-bind:state="home.address.state_code"
+              v-bind:postal-code="home.address.postal_code">
             </widget-location-detail>
           </div>
         </router-link>
-        <div v-if="home.name" class="my-1 pt-2 border-t">
+        <div 
+          v-if="home.branding.listing_office.list_item.name" 
+          class="my-1 pt-2 border-t">
           <div class="uppercase pb-2 text-xs text-gray-600">
-            LISTING BY: {{ home.name }} | Long: {{ home.longitude }} | Lat: 
-            {{ home.latitude }}
+            LISTING BY: {{ home.branding.listing_office.list_item.name }} | Long: {{ home.address.lon }} | Lat: 
+            {{ home.address.lat }}
           </div>
         </div>
       </div>
@@ -52,7 +56,7 @@
   </li>
 </template>
 <script>
-import { computed } from "vue";
+// import { computed } from "vue";
 import WidgetUtilitySummary from "@/components/WidgetUtilitySummary.vue";
 import WidgetImageSlider from "@/components/WidgetImageSlider.vue";
 import WidgetPriceDetail from "@/components/WidgetPriceDetail.vue";
@@ -70,12 +74,12 @@ export default {
     WidgetPriceDetail,
     WidgetLocationDetail,
   },
-  setup(props) {
-    const homeCity = computed(() => props.home.city.replaceAll(" ", "-"));
-    const homeStreet = computed(() => props.home.street.replaceAll(" ", "-"));
+  setup() {
+    // const homeCity = computed(() => props.home.city.replaceAll(" ", "-"));
+    // const homeStreet = computed(() => props.home.street.replaceAll(" ", "-"));
     return{
-      homeCity,
-      homeStreet,
+      // homeCity: computed(() => props.home.address.city.replaceAll(" ", "-")),
+      // homeStreet: computed(() => props.home.address.line.replaceAll(" ", "-")),
     }
   }
 }
