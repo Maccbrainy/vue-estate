b6208327f3ec5c7d3f4e92a01d246c081c02411c
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-19 14:12:07 +0100

    Router parameters updated and checks implemented for undefined variables

--- src/composables/buildRouterParamsUrl.js
+++ src/composables/buildRouterParamsUrl.js
@@ -1,41 +1,49 @@
 import router from "@/router";
 import { ref } from "vue";
-export default function buildRouterParamsUrl(
-  validatedSearchInfo, 
-  propsCity, 
-  // hyphenFreeProp,
-) {
+export default function buildRouterParamsUrl(payLoad) {
+  
+  const searchParams = ref("");
+  searchParams.value = payLoad;
 
-  console.log("I am working!");
-  const searchSlugTerm = ref("");
-  searchSlugTerm.value = validatedSearchInfo;
-  if (!searchSlugTerm.value) return;
-  //Regular Expression for numeric characters only thus to detect postal or zip codes search;
+  if (!searchParams.value) return;
+
+  let cityParam = 
+    searchParams.value.city != undefined ? searchParams.value.city : "";
+  let slugParam = 
+    searchParams.value.state_code != undefined
+      ? searchParams.value.state_code
+      : searchParams.value;
+  /**Regular Expression
+   * for numeric characters only 
+   * thus to detect postal or zip codes search; 
+   * */
   const regExpNumbersOnly = /^\d+$/;
-  // const regExpWhiteSpaces = /\s/gi;
-  // const replacedWhiteSpacesInPropsCityWithHypen = regExpWhiteSpaces.test(propsCity)
-  //   ? propsCity.replaceAll(" ", "-")
-  //   : propsCity;
-  const searchedWithZipCode = `Homes For Sale & Real Estate in ${searchSlugTerm.value} Zip Code`;
-  //const searchedWithCityNameDefined = `${hyphenFreeProp} Homes For Sale & ${hyphenFreeProp} Real Estate | Vue Estate`;
-  const searchedDefault = `${searchSlugTerm.value} Homes For Sale & ${searchSlugTerm.value} Real Estate | Vue Estate`;
-  //named route with params to let the router build the url;
+  const titleParam = regExpNumbersOnly.test(slugParam) 
+    ? searchedWithZipCode 
+    : cityParam 
+    ? searchedWithCityNameDefined
+    : searchedDefault
+  /**Regular Expression
+   * for white space detections only  
+  * */
+  const regExpWhiteSpaces = /\s/gi;
+  const removedWhiteSpacesFromCityParam = regExpWhiteSpaces.test(cityParam)
+    ? cityParam.replaceAll(" ", "_")
+    : cityParam;
+
+  const searchedWithZipCode = `Homes For Sale & Real Estate in ${slugParam} Zip Code`;
+  const searchedWithCityNameDefined = `${cityParam} Homes For Sale & ${cityParam} Real Estate | Vue Estate`;
+  const searchedDefault = `${slugParam} Homes For Sale & ${slugParam} Real Estate | Vue Estate`;
+
   router.push({
     name: "BuyPage",
     params: {
-      slug: searchSlugTerm.value,
-      city: propsCity ? propsCity : "",
-      title: regExpNumbersOnly.test(searchSlugTerm.value) 
-        ? searchedWithZipCode 
-        : searchedDefault,
-      // title: regExpNumbersOnly.test(searchSlugTerm.value) 
-      //   ? searchedWithZipCode 
-      //   : hyphenFreeProp 
-      //   ? searchedWithCityNameDefined
-      //   : searchedDefault,
+      slug: slugParam,
+      city: removedWhiteSpacesFromCityParam,
+      title: titleParam,
     },
   });
   return {
-    searchTerm: searchSlugTerm,
+    searchTerm: searchParams,
   }
 }
