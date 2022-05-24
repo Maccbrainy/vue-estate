eb8626a3a60330b5195e0ccca174fcbabf9ecc9b
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-24 13:50:09 +0100

    Fall through switch statement to get the appropriate route state for the home page

--- src/assets/icons/VueEstateLogo.vue
+++ src/assets/icons/VueEstateLogo.vue
@@ -4,7 +4,7 @@
   </div>
 </template>
 <script>
-import { computed } from "vue";
+import { ref, computed, watchEffect } from "vue";
 import { useRouter } from "vue-router"
 import { useStore } from "vuex";
 
@@ -12,17 +12,29 @@ export default {
   setup() {
     const router = useRouter();
     const store = useStore();
+    const activeRouteTab = ref("");
 
     const getIsActiveRouteTab = computed(() => {
       return store.getters.getIsActiveRouteTab;
     });
+    watchEffect(() => {
+      switch (getIsActiveRouteTab.value) {
+        case "RentPage":
+        case "list-for-rent":
+          activeRouteTab.value = "list-for-rent"
+          break;
+        case "SoldPage":
+        case "list-sold":
+          activeRouteTab.value = "list-sold"
+          break;
+        default:
+          activeRouteTab.value = "HomePage"
+          break;
+      }
+    });
     function returnHomeAndKeepRouteState(){
-      console.log("Home clicked");
       router.push({
-        name: 
-          getIsActiveRouteTab.value == "list-for-sale" 
-            ? "HomePage" 
-            : getIsActiveRouteTab.value
+        name: activeRouteTab.value
       });
     }
     return {
