8ee9a9839e3cda782d96928f64d1d48d1781ea6d
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-28 19:58:25 +0100

    Mobile responsive and update

--- src/views/TheHomePage.vue
+++ src/views/TheHomePage.vue
@@ -29,9 +29,9 @@
                   w-full 
                   h-full 
                   items-center 
-                  top-0">
+                  top-4">
                 <div class="m-0">
-                  <div class="text-5xl font-bold shadow">
+                  <div class="text-5xl sf:text-3xl font-bold shadow">
                     <h1> 
                       Discover a place <br /> 
                       you'll love to live
@@ -41,22 +41,50 @@
                 <div class="mt-16 mb-3">
                   <home-tab-buttons></home-tab-buttons>
                 </div>
-                <search-box class="w-5/12 text-xl">
+                <search-box class="w-6/12 xs:w-11/12 sf:w-10/12 text-xl">
                   <search-input class="h-16"></search-input>
                 </search-box>
               </div>
             </div>
-            <div class="w-5/6 my-8 mx-auto flex flex-col text-center">
-              <div class="text-4xl text-gray-700 font-bold">
-                <h1>See how Vue Estate App can help</h1></div>
-              <div class="flex flex-row space-x-4 justify-center">
-                <div>Rent a home {{ userLocLat }} {{ userLocLong }}</div>
-                <div>Buy a home</div>
-                <div>See neighborhoods</div>
-              </div>
-            </div>
           </div>
         </div>
+      </div>
+      <div class="w-5/6 m-auto flex flex-col text-center relative top-32">
+        <div class="text-4xl sf:text-3xl sf:w-10/12 m-auto text-gray-700 font-bold">
+          <h1>See how Vue Estate App can help</h1>
+        </div>
+        <div class="flex sf:block space-x-4 justify-center mt-10 w-9/12 m-auto">
+          <div class="flex flex-col items-center max-w-xs p-2 cursor-pointer">
+            <buy-a-home />
+            <h1 class="text-gray-700 text-2xl font-semibold">Buy a Home</h1>
+            <p class="font-normal text-base text-gray-600 mt-3 mb-5">With over 1 million+ homes for sale available on the website, Trulia can match you with a house you will want to call home.</p>
+            <button 
+              type="button" 
+              class="bg-teal px-4 py-2 shadow-xl text-white text-base font-bold rounded-lg border border-teal hover:bg-transparent hover:text-teal focus:bg-teal-lighter focus:text-teal">
+                <router-link to="/houses-for-sale-near-me">Find a Home</router-link>
+          </button>
+          </div>
+          <div class="flex flex-col items-center max-w-xs p-2">
+            <rent-a-home />
+            <h1 class="text-gray-700 text-2xl font-semibold">Rent a Home</h1>
+            <p class="font-normal text-base text-gray-600 mt-3 mb-3">With 35+ filters and custom keyword search, Trulia can help you easily find a home or apartment for rent that you'll love.</p>
+            <button>Find a Rental</button>
+          </div>
+          <div class="flex flex-col items-center max-w-xs p-2">
+            <neighbor-hoods />
+            <h1 class="text-gray-700 text-2xl font-semibold">See Neighborhoods</h1>
+            <p class="font-normal text-base text-gray-600 mt-3 mb-3">With more neighborhood insights than any other real estate website, we've captured the color and diversity of communities.</p>
+            <button>Learn more</button>
+          </div>
+        </div>
+      </div>
+      <div class="w-full my-8 mx-auto flex flex-col justify-center items-center relative top-40">
+        <div class="mb-6">
+          <h1 class="text-gray-700 text-xl font-semibold">Check out a neighborhood</h1>
+        </div>
+        <search-box class="w-6/12 xs:w-11/12 sf:w-10/12 text-xl">
+          <search-input class="h-14 bg-gray-100"></search-input>
+        </search-box>
       </div>
     </template>
     <template v-slot:footer>
@@ -65,7 +93,10 @@
 </template>
 <script>
 import { useStore } from "vuex";
-import { ref, computed, watch, onMounted, reactive } from "vue";
+import { ref, computed, watch, onMounted, reactive, watchEffect } from "vue";
+import BuyAHome from "@/assets/icons/BuyAHome.vue";
+import RentAHome from "@/assets/icons/RentAHome.vue";
+import NeighborHoods from "@/assets/icons/Neighborhoods.vue";
 import { SearchBox, SearchInput } from "@/components/buttonui";
 import NavBarContainer from "@/components/NavBarContainer.vue";
 import HomeTabButtons from "@/components/HomeTabButtons.vue";
@@ -81,15 +112,35 @@ export default {
     SearchInput,
     HomePageLayout,
     HomeTabButtons,
+    BuyAHome,
+    RentAHome,
+    NeighborHoods
     // ButtonBox,
     // SearchButton,
   },
   setup() {
     const store = useStore();
+    // const router = useRouter();
     const stateSearchedData = ref("");
+    const userFindSwitch = ref("");
     const userLocLat = ref("");
     const userLocLong = ref("");
-    // const userLatLong = reactive({})
+    const findHomeAndRental = [
+      {
+        id: "BuyHome",
+        title: "Find a Home",
+      },
+      {
+        id: "RentalHome",
+        title: "Find a Rental",
+      }
+    ];
+    const neighborhoods = [
+      {
+        id: "Neighborhoods",
+        title: "Learn more",
+      }];
+
     const searchedDataFromStore = computed(() => {
       return store.getters.getSearchedData
     });
@@ -98,9 +149,20 @@ export default {
       stateSearchedData.value = val;
     });
     const { cordinates, userEnabledLocation } = userGeolocation();
-
-    // console.log("From Destructured:", cordinates);
-
+    const getIsActiveRoutePath = computed(() => {
+      return store.getters.getIsActiveRouteTab;
+    });
+    watchEffect(() => {
+      switch (getIsActiveRoutePath.value) {
+        case "list-for-rent":
+          userFindSwitch.value = findHomeAndRental.reverse()
+          break;
+        default:
+          userFindSwitch.value = findHomeAndRental;
+          break;
+      }
+    });
+    const userSearchPriority = ref([...userFindSwitch.value, ...neighborhoods]);
     onMounted(() => {
       navigator.geolocation.getCurrentPosition(
         ({ coords: { latitude, longitude } }) => {
@@ -114,12 +176,13 @@ export default {
         })
     });
     return {
+      userSearchPriority,
       userLocLat,
       userLocLong,
       cordinates,
       searchedDataFromStore,
       stateSearchedData,
-      userEnabledLocation
+      userEnabledLocation,
     }
   }
 };
