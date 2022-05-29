1a2b02a9a7700a1edbdeedfb532c5f18678f8395
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-05-29 06:32:08 +0100

    Code refactor and dynamic component rendering in accordance of the app state

--- src/views/TheHomePage.vue
+++ src/views/TheHomePage.vue
@@ -6,7 +6,7 @@
       </nav-bar-container>
     </template>
     <template v-slot:default>
-      <div class="px-4">
+      <section class="px-4">
         <div class="flex flex-col overflow-visible">
           <div class="relative -bottom-16">
             <div class="w-full h-100 overflow-hidden">
@@ -48,44 +48,49 @@
             </div>
           </div>
         </div>
-      </div>
-      <div class="w-5/6 m-auto flex flex-col text-center relative top-32">
+      </section>
+      <section class="w-5/6 m-auto flex flex-col text-center relative top-32">
         <div class="text-4xl sf:text-3xl sf:w-10/12 m-auto text-gray-700 font-bold">
           <h1>See how Vue Estate App can help</h1>
         </div>
         <div class="flex sf:block space-x-4 justify-center mt-10 w-9/12 m-auto">
-          <div class="flex flex-col items-center max-w-xs p-2 cursor-pointer">
-            <buy-a-home />
-            <h1 class="text-gray-700 text-2xl font-semibold">Buy a Home</h1>
-            <p class="font-normal text-base text-gray-600 mt-3 mb-5">With over 1 million+ homes for sale available on the website, Trulia can match you with a house you will want to call home.</p>
-            <button 
-              type="button" 
-              class="bg-teal px-4 py-2 shadow-xl text-white text-base font-bold rounded-lg border border-teal hover:bg-transparent hover:text-teal focus:bg-teal-lighter focus:text-teal">
-                <router-link to="/houses-for-sale-near-me">Find a Home</router-link>
-          </button>
-          </div>
-          <div class="flex flex-col items-center max-w-xs p-2">
-            <rent-a-home />
-            <h1 class="text-gray-700 text-2xl font-semibold">Rent a Home</h1>
-            <p class="font-normal text-base text-gray-600 mt-3 mb-3">With 35+ filters and custom keyword search, Trulia can help you easily find a home or apartment for rent that you'll love.</p>
-            <button>Find a Rental</button>
-          </div>
-          <div class="flex flex-col items-center max-w-xs p-2">
-            <neighbor-hoods />
-            <h1 class="text-gray-700 text-2xl font-semibold">See Neighborhoods</h1>
-            <p class="font-normal text-base text-gray-600 mt-3 mb-3">With more neighborhood insights than any other real estate website, we've captured the color and diversity of communities.</p>
-            <button>Learn more</button>
+          <div 
+            v-for="priority in userSearchPriorities"
+            :key="priority.id" 
+            class="flex flex-col items-center max-w-xs p-2 cursor-pointer">
+            <component v-bind:is="priority.icon"></component>
+            <h1 class="text-gray-700 text-2xl font-semibold">
+              {{ priority.title }}</h1>
+            <p class="font-normal text-base text-gray-600 mt-3 mb-5">
+              {{ priority.description }}</p>
+            <router-link :to="priority.url">
+              <button 
+                type="button" 
+                class="
+                bg-teal 
+                  px-4 
+                  py-2 
+                  shadow-xl 
+                  text-white text-base 
+                  font-bold 
+                  rounded-lg 
+                  border border-teal 
+                  hover:bg-transparent hover:text-teal 
+                  focus:bg-teal-lighter focus:text-teal">
+                {{ priority.callToAction }}
+              </button>
+            </router-link>
           </div>
         </div>
-      </div>
-      <div class="w-full my-8 mx-auto flex flex-col justify-center items-center relative top-40">
+      </section>
+      <section class="w-full my-8 mx-auto flex flex-col justify-center items-center relative top-40">
         <div class="mb-6">
           <h1 class="text-gray-700 text-xl font-semibold">Check out a neighborhood</h1>
         </div>
         <search-box class="w-6/12 xs:w-11/12 sf:w-10/12 text-xl">
           <search-input class="h-14 bg-gray-100"></search-input>
         </search-box>
-      </div>
+      </section>
     </template>
     <template v-slot:footer>
     </template>
@@ -93,7 +98,15 @@
 </template>
 <script>
 import { useStore } from "vuex";
-import { ref, computed, watch, onMounted, reactive, watchEffect } from "vue";
+import { 
+  ref, 
+  computed,
+  watch, 
+  onMounted, 
+  reactive, 
+  watchEffect, 
+  markRaw 
+} from "vue";
 import BuyAHome from "@/assets/icons/BuyAHome.vue";
 import RentAHome from "@/assets/icons/RentAHome.vue";
 import NeighborHoods from "@/assets/icons/Neighborhoods.vue";
@@ -112,33 +125,45 @@ export default {
     SearchInput,
     HomePageLayout,
     HomeTabButtons,
-    BuyAHome,
-    RentAHome,
-    NeighborHoods
-    // ButtonBox,
-    // SearchButton,
   },
   setup() {
     const store = useStore();
     // const router = useRouter();
     const stateSearchedData = ref("");
     const userFindSwitch = ref("");
+    const userSearchPriorities = ref([]);
     const userLocLat = ref("");
     const userLocLong = ref("");
+    const { cordinates, userEnabledLocation } = userGeolocation();
+    const buyHomeIcon = markRaw(BuyAHome);
+    const rentHomeIcon = markRaw(RentAHome);
+    const neighborhoodIcon = markRaw(NeighborHoods);
     const findHomeAndRental = [
       {
         id: "BuyHome",
-        title: "Find a Home",
+        title: "Buy a Home",
+        description: "With over 1 million+ homes for sale available on the website, Vue Estate App can match you with a house you will want to call home.",
+        icon: buyHomeIcon,
+        url: "/houses-for-sale-near-me/",
+        callToAction: "Find a Home"
       },
       {
         id: "RentalHome",
-        title: "Find a Rental",
+        title: "Rent a Home",
+        description: "With 35+ filters and custom keyword search, Vue Estate App can help you easily find a home or apartment for rent that you'll love.",
+        icon: rentHomeIcon,
+        url: "/apartments-for-rent-near-me/",
+        callToAction: "Find a Rental"
       }
     ];
     const neighborhoods = [
       {
         id: "Neighborhoods",
-        title: "Learn more",
+        title: "See Neighborhoods",
+        description: "With more neighborhood insights than any other real estate website, we've captured the color and diversity of communities",
+        icon: neighborhoodIcon,
+        url: "/neighborhoods/",
+        callToAction: "Learn more"
       }];
 
     const searchedDataFromStore = computed(() => {
@@ -148,10 +173,11 @@ export default {
     watch(searchedDataFromStore, (val) => {
       stateSearchedData.value = val;
     });
-    const { cordinates, userEnabledLocation } = userGeolocation();
+
     const getIsActiveRoutePath = computed(() => {
       return store.getters.getIsActiveRouteTab;
     });
+
     watchEffect(() => {
       switch (getIsActiveRoutePath.value) {
         case "list-for-rent":
@@ -161,8 +187,8 @@ export default {
           userFindSwitch.value = findHomeAndRental;
           break;
       }
+      userSearchPriorities.value = [...userFindSwitch.value, ...neighborhoods];
     });
-    const userSearchPriority = ref([...userFindSwitch.value, ...neighborhoods]);
     onMounted(() => {
       navigator.geolocation.getCurrentPosition(
         ({ coords: { latitude, longitude } }) => {
@@ -176,7 +202,7 @@ export default {
         })
     });
     return {
-      userSearchPriority,
+      userSearchPriorities,
       userLocLat,
       userLocLong,
       cordinates,
