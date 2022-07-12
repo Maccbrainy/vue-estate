9fa1b10a65eb9b01a410117ea55a8d75ae8ca7b1
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-07-12 13:37:47 +0100

    Feat: Endpoint api comsuption for image

--- src/components/WidgetImageSlider.vue
+++ src/components/WidgetImageSlider.vue
@@ -1,14 +1,26 @@
 <template>
-  <div class="relative w-full h-44 bg-gray-200 rounded-xl overflow-hidden">
-    <!-- <img :src="callout.imageSrc" :alt="callout.imageAlt" class="w-full h-full object-center object-cover" /> -->
-    <img 
-      src="../assets/images/HomePage.jpg" 
+  <div
+    v-bind:class="{ 'animate-pulse': !propertyListingPhotos }" 
+    class="relative w-full h-44 bg-gray-200 rounded-xl overflow-hidden">
+    <img
+      v-bind:class="{ 'hidden': !propertyListingPhotos }" 
+      v-bind:src="listingPhotos" 
       alt="" 
-      class="w-full h-full object-center object-cover" />
+      class="w-full h-full object-center object-cover"/>
   </div>
 </template>
 <script>
+import { computed } from "vue";
 export default {
   name: "WidgetImageSlider",
+  props: ["listingPhotos"],
+  setup(props){
+    const propertyListingPhotos = computed(() => {
+      return !props.listingPhotos ? false : true
+    })
+    return {
+      propertyListingPhotos
+    }
+  }
 }
 </script>
