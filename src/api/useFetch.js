891c7d24d9dfbc48706867ccc9939ee0fbf0f97a
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-07-12 13:32:11 +0100

    Composable for api data fetch

--- src/api/useFetch.js
+++ src/api/useFetch.js
@@ -1,13 +1,34 @@
 import { ref } from "vue";
 // import { useStore } from "vuex";
-// import axios from "axios";
-export async function useFetch (routName, slug, city){
+import axios from "axios";
+export async function useFetch(routeName, slug, city){
   const propertyDetail = ref(null);
   const error = ref(null);
-  propertyDetail.value = {
-    routeName: routName,
-    slug: slug,
-    cityName: city.replace(/_/g, " ")  
+  // propertyDetail.value = {
+  //   routeName: routeName,
+  //   slug: slug,
+  //   cityName: city  
+  // };
+  try {
+    const response = await axios.get(
+      `https://realty-in-us.p.rapidapi.com/properties/v2/${routeName}`, 
+      { 
+        params: {
+          city: city ? city : slug,
+          state_code: slug,
+          offset: "0",
+          limit: "200",
+          sort: "relevance"
+        },
+        headers: {
+          "X-RapidAPI-Host": "realty-in-us.p.rapidapi.com",
+          "X-RapidAPI-Key": `${process.env.VUE_APP_RAPID_API_KEY}`
+        }
+      });
+    propertyDetail.value = response;
+  } catch (e){
+    error.value = true;
+    console.log("Error in fetching from rapid api:", e) 
   }
   return {
     error,
