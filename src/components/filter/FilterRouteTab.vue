936c365cf3180da99f2f005838e8f9d67db1a691
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-07-14 17:11:06 +0100

    Feature: Check for and replace whitespaces, get appropriate app route

--- src/components/filter/FilterRouteTab.vue
+++ src/components/filter/FilterRouteTab.vue
@@ -41,25 +41,41 @@ export default {
     //   return store.getters.getSearchedData;
     // });
     watchEffect(() => {
-      if (getIsActiveRouteTab.value == ""){
-        store.commit("setActiveRoutePath", route.name);
-        activeRouteTemplate.value = route.name;
+      // if (getIsActiveRouteTab.value == ""){
+      switch (getIsActiveRouteTab.value) {
+        case "SalesNearMe":
+          store.commit("setActiveRoutePath", "BuyPage");
+          activeRouteTemplate.value = "BuyPage";
+          break;
+        case "RentsNearMe":
+          store.commit("setActiveRoutePath", "RentPage");
+          activeRouteTemplate.value = "RentPage";
+          break;
+        default:
+          store.commit("setActiveRoutePath", getIsActiveRouteTab.value);
+          activeRouteTemplate.value = getIsActiveRouteTab.value;
+          break;
       }
-      activeRouteTemplate.value = getIsActiveRouteTab.value;
+      // }
+      // activeRouteTemplate.value = getIsActiveRouteTab.value;
     });
 
     function setPropertyListingTemplate(e){
       let routeName = e.target.id;
       let isActiveRoute = getIsActiveRouteTab.value;
-      if (routeName != isActiveRoute){
-        searchPayload.value = {
-          city: route.params.city,
-          state_code: route.params.slug,
-          activeRouteTab: routeName,
-        };
-        store.commit("setUseRouterPush", searchPayload.value);
-        store.commit("setSearchedData", searchPayload.value);
-      }
+      let cityMayContainUnderscore = `${route.params.city}`;
+      let cityParam = cityMayContainUnderscore.replace(/_/g," ");
+      
+      if (routeName == isActiveRoute){
+        return;
+      };
+      searchPayload.value = {
+        city: !route.params.city ? "San Francisco" : cityParam,
+        state_code: !route.params.slug ? "CA" : route.params.slug,
+        activeRouteTab: routeName,
+      };
+      store.commit("setUseRouterPush", searchPayload.value);
+      store.commit("setSearchedData", searchPayload.value);
     };
     return {
       activeRouteTemplate,
