21d33abe20ea61f454df93d815fbe81cd42610ab
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-07-14 16:56:52 +0100

    Disable hover effect when app is loading

--- src/components/buttonui/NavBarDropdown.vue
+++ src/components/buttonui/NavBarDropdown.vue
@@ -1,10 +1,10 @@
 <template>
   <ul
+    v-bind:class="{ 'group-hover:block': !isLoading}"
     class="
       z-30
       top-10
       left-0
-      group-hover:block
       absolute 
       pb-1 pt-1
       w-48
@@ -18,3 +18,23 @@
     <slot></slot>
   </ul>
 </template>
+<script>
+import { useStore } from "vuex";
+import { ref, computed, watchEffect } from "vue";
+export default {
+  setup() {
+    const store = useStore();
+    const isLoading = ref(false);
+    const loadingIsActive = computed(() => {
+      return store.getters.getIsLoading;
+    });
+    watchEffect(() => {
+      isLoading.value = loadingIsActive.value;
+    });
+    return {
+      isLoading,
+    }
+  },
+}
+
+</script>
