98438a4bef96fbc4435ddaefeabfd8283d6e6f1e
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-07-14 17:20:49 +0100

    Feature: Thumbnail display for homepage

--- src/views/TheHomePage.vue
+++ src/views/TheHomePage.vue
@@ -49,33 +49,31 @@
           </div>
         </div>
       </section>
-      <section class="w-full relative top-32 mb-20">
-        <div class="overflow-hidden">
+      <section class="w-full relative top-32 mb-20 hidden">
+        <div class="">
           <button
             v-on:click="moveSlides(-1)" 
             class="absolute z-10 left-0 top-1/2"
             :class="{'hidden': moveIndex == 0}">
               <chevron-left class=" text-gray-800 w-10 h-10 p-2 ml-3 bg-white rounded-full shadow-md hover:bg-gray-100 active:bg-gray-300"/>
             </button>
-          <button
-            class="absolute z-10 right-0 top-1/2"
-            :class="{'hidden': moveIndex == 2}">
+          <button v-if="moveIndex != -300"
+            class="absolute z-10 right-0 top-1/2">
               <chevron-right v-on:click="moveSlides(1)" class="text-gray-800 w-10 h-10 p-2 mr-3 bg-white rounded-full shadow-md hover:bg-gray-100 active:bg-gray-300"/>
             </button>
-          <div class="snap-mandatory px-2 box-border">
-            <ul
+          <div class="flex">
+            <!-- <ul
               v-for="(groupedItem, index) in groupedItems" 
               v-bind:key="groupedItem"
+              v-bind:ref="el => itemsUl[index] = el"
               v-bind:id="`grouped-item-${index}`" 
-              v-bind:class="{
-                'flex': moveIndex == index,
-              }">
+              class="flex flex-nowrap duration-700">
               <search-result-item-card 
                 v-for="(home, i) in groupedItem"
                 v-bind:key="home.property_id || home.id"
                 v-bind:ref="el => itemRefs[i] = el"
                 v-bind:home="home"
-                v-bind:class="{'hidden': moveIndex != index}">
+                class="duration-700">
               </search-result-item-card>
               <li class="
                 block 
@@ -89,29 +87,118 @@
                 xl:w-6/12 
                 2xl:w-4/12
                 3xl:w-3/12"
-                v-if="groupedItem.length < 5" 
-                v-bind:class="{'hidden': moveIndex != index}">
+                 
+                v-bind:class="{'hidden': moveIndex !== -300}">
                   <div class="relative w-full h-44 bg-gray-200 rounded-xl overflow-hidden">
                     See more homes for sale
                   </div>
                 </li>
-            </ul>
+            </ul> -->
           </div>
         </div>
       </section>
-      <section class="max-w-6xl mf:w-full m-auto flex flex-col text-center relative top-32">
-        <div class="text-4xl sf:text-3xl sf:w-10/12 m-auto text-gray-700 font-bold">
+      <section class="hidden w-full relative box-border top-32 overflow-hidden">
+        <button
+        v-if="moveIndex < 0"
+          v-on:click="moveSlides(-1)" 
+          class="absolute z-10 left-0 top-1/2">
+          <chevron-left 
+            class=" text-gray-800 w-8 h-8 p-2 ml-3 bg-white rounded-full shadow-md transform hover:scale-105 active:bg-gray-300"/>
+        </button>
+        <button
+          v-if="moveIndex != -1000"
+          class="absolute z-10 right-0 top-1/2">
+          <chevron-right 
+            v-on:click="moveSlides(1)" 
+            class="
+              text-gray-800 
+              w-8 
+              h-8 
+              p-2 
+              mr-3 
+              bg-white 
+              rounded-full 
+              shadow-md 
+              transform 
+              hover:scale-105 
+              active:bg-gray-300"/>
+        </button>
+        <ul class="flex flex-nowrap">
+          <!-- <search-result-item-card 
+            v-for="(home, i) in reference"
+            v-bind:key="home.property_id || home.id"
+            v-bind:ref="el => itemRefs[i] = el"
+            v-bind:home="home"
+            class="duration-700">
+          </search-result-item-card> -->
+          <li
+            v-if="moveIndex == -1000"
+            class="
+              block 
+              flex-initial
+              border-solid border-transparent border-r-8 border-l-8 border-t-8 
+              pb-4
+              xs:w-full 
+              sm:w-full 
+              md:w-full 
+              lg:w-6/12 
+              xl:w-6/12 
+              2xl:w-4/12
+              3xl:w-3/12">
+            <div 
+              class="
+                relative 
+                w-full 
+                h-44 
+                bg-gray-200 
+                rounded-xl 
+                overflow-hidden">
+                See more homes for sale
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section 
+        class="
+          max-w-6xl 
+          mf:w-full 
+          m-auto 
+          flex flex-col 
+          text-center 
+          relative 
+          top-32">
+        <div 
+          class="text-4xl sf:text-3xl sf:w-10/12 m-auto text-gray-700 font-bold">
           <h1>See how Vue Estate App can help</h1>
         </div>
-        <div class="flex sf:grid space-x-4 justify-center mt-10 w-9/12 lf:w-11/12 m-auto">
+        <div 
+          class="
+            flex 
+            sf:grid 
+            space-x-4 
+            justify-center 
+            mt-10 
+            w-9/12 
+            lf:w-11/12 
+            m-auto">
           <call-to-action-card 
             v-bind:callToActionsData="searchNearMeModel">
           </call-to-action-card>
         </div>
       </section>
-      <section class="w-full my-8 mx-auto flex flex-col justify-center items-center relative top-40">
+      <section 
+        class="
+          w-full 
+          my-8 
+          mx-auto 
+          flex flex-col 
+          justify-center 
+          items-center 
+          relative 
+          top-40">
         <div class="mb-6">
-          <h1 class="text-gray-700 text-xl font-semibold">Check out a neighborhood</h1>
+          <h1 class="text-gray-700 text-xl font-semibold"> 
+            Check out a neighborhood</h1>
         </div>
         <search-box class="w-6/12 xs:w-11/12 sf:w-10/12 text-xl">
           <search-input class="h-14 bg-gray-100"></search-input>
@@ -157,7 +244,7 @@ import CallToActionCard from "@/components/CallToActionCard.vue";
 import HomePageLayout from "@/layouts/HomePageLayout.vue";
 import userGeolocation from "@/helper/userGeolocation";
 import jsonProperties from "@/api/autoComplete.json";
-import SearchResultItemCard from "@/components/SearchResultItemCard.vue";
+// import SearchResultItemCard from "@/components/SearchResultItemCard.vue";
 import ExperimentalJson from "@/components/homes.json";
 import ChevronRight from "@/assets/icons/ChevronRight.vue";
 import ChevronLeft from "@/assets/icons/ChevronLeft.vue";
@@ -172,7 +259,7 @@ export default {
     HomeTabButtons,
     ShowHideListColumns,
     CallToActionCard,
-    SearchResultItemCard,
+    // SearchResultItemCard,
     ChevronRight,
     ChevronLeft
   },
@@ -304,16 +391,42 @@ export default {
     const expJson = ref(ExperimentalJson.homes);
     const groupedItems = ref({});
     const itemRefs = ref([]);
+    const reference = ref([]);
     const moveIndex = ref(0);
+    const itemsUl = ref([]);
 
-    function moveSlides(value){
-      if(value == 1 && moveIndex.value < 2){
-        moveIndex.value++;
-      };
-      if(value == -1 && moveIndex.value > 0){
-        moveIndex.value--;
-      }
+    const translateAmount = ref(500); 
+
+    reference.value = expJson.value.slice(0,13);
+
+    const endOfSlide = computed(() => {
+      return expJson.value.length >= 13 ? true : false;
+    });
+    function moveSlides(direction){
+      let x =(window.innerWidth /2);
+      let y =(x - 183);
+      // console.log("ItemsRefs Length:", itemRefs.value.length);
+      // // console.log("Inner Width:", window.innerWidth);
+      // // console.log("itemRefs scrollWidth:", itemRefs.value.$el.scrollWidth);
+      // // console.log("itemRefs clientWidth:", itemRefs.value.$el.clientWidth);
+      // console.log("itemRefs getBoundingClientRect:", itemRefs.value.$el.offsetWidth);
+      // direction === 1 
+      //   ? moveIndex.value  -= 100 
+      //   : moveIndex.value  += 100;
+
+      // itemsUl.value.forEach( 
+      //   pages => ( pages.attributes[0].ownerElement.style.transform = `translateX(${moveIndex.value}%)`));
+
+      // console.log("moveIndex:", moveIndex.value);
+
+      direction === 1 
+        ? moveIndex.value  -= y 
+        : moveIndex.value  += y; 
+
+      itemRefs.value.forEach( 
+        pages => ( pages.$el.style.transform = `translateX(${moveIndex.value}%)`));
     };
+    
 
     onMounted(() => {
       groupItems()
@@ -331,16 +444,22 @@ export default {
     function groupItems(){
       let groupedItemsByIndex = {
         0: expJson.value.slice(0, 5),
-        1: expJson.value.slice(4, 9),
-        2: expJson.value.slice(8, 12)
+        1: expJson.value.slice(6, 11),
+        2: expJson.value.slice(12, 15)
       };
       groupedItems.value = groupedItemsByIndex;
+      // reference.value = expJson.value.slice(0,13);
+      
   };
 
     return {
       itemRefs,
+      itemsUl,
+      reference,
       expJson,
-      groupedItems,
+      translateAmount,
+      // groupedItems,
+      endOfSlide,
       searchNearMeModel,
       userLocLat,
       userLocLong,
@@ -361,7 +480,4 @@ export default {
 .h-100 {
   height: 27.7rem;
 }
-/* .transX {
-  transition: all 0.7s;
-} */
 </style>
