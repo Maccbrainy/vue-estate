b9952b2c5c368a9d0f9f0b371fa74b8f5ecb092c
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-09-13 18:10:30 +0100

    Feat: User location cordinate finder

--- src/components/CallToActionCard.vue
+++ src/components/CallToActionCard.vue
@@ -10,8 +10,13 @@
     <p class="font-normal text-base text-gray-600 mt-3 mb-5">
       {{ callTo.description }}
     </p>
-    <router-link :to="callTo.url">
-      <button 
+    <router-link 
+      :to="{ 
+        name: callTo.urlId
+      }">
+      <button
+        v-on:click="setLongitudeAndLatitude"
+        :id="callTo.urlId" 
         type="button" 
         class="
           bg-teal 
@@ -33,6 +38,10 @@
 import BuyAHomeIcon from "@/assets/icons/BuyAHome.vue";
 import RentAHomeIcon from "@/assets/icons/RentAHome.vue";
 import NeighborHoodsIcon from "@/assets/icons/Neighborhoods.vue";
+
+import { reactive } from "vue";
+// import { useStore } from "vuex";
+// import { useRouter } from "vue-router";
 export default {
   name: "CallToActionCard",
   components: {
@@ -44,7 +53,30 @@ export default {
     callToActionsData: Array
   },
   setup() {
-    return {}
+    // const router = useRouter();
+    // const store = useStore();
+    const setLongitudeAndLatitude = () => {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords: { latitude, longitude } }) => {
+          const userCordinates = reactive({
+            lat: latitude,
+            long: longitude,
+          })
+          console.log("From CallToAction", userCordinates);
+          // let latitudeFilter = `lat=${userCordinates.lat}&`;
+          // let longitudeFilter = `long=${userCordinates.long}`;
+          // router.push({
+          //   name: e.target.id,
+          //   params: {
+          //     lat: latitudeFilter,
+          //     long: longitudeFilter,
+          //   }
+          // })
+        });
+    }
+    return {
+      setLongitudeAndLatitude
+    }
   },
 }
 </script>
