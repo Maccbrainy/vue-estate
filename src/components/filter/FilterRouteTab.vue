fa6c07deebb151d2855adce5a3902324e853adad
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-09-26 10:50:27 +0100

    Fix: Updating the activeRoutePath and searchedData store instantly to fix the filter button hanging/bugs when switching to a different route

--- src/components/filter/FilterRouteTab.vue
+++ src/components/filter/FilterRouteTab.vue
@@ -1,12 +1,13 @@
 <template>
-  <button-group-multi-buttons 
+  <button-group-multi-buttons
     v-bind:options="propertyRouteTemplate"
-    v-bind:isActiveTab="activeRouteTemplate"
-    v-on:getOptionId="setPropertyListingTemplate">
+    v-bind:isActiveTab="storeData.activeRoutePath"
+    v-on:getOptionId="setPropertyListingTemplate"
+  >
   </button-group-multi-buttons>
 </template>
 <script>
-import { ref, computed, watchEffect } from "vue";
+import { ref, computed } from "vue";
 import { useStore } from "vuex";
 import { useRoute } from "vue-router";
 import { ButtonGroupMultiButtons } from "@/components/buttonui";
@@ -14,60 +15,45 @@ import settingsData from "@/api/settingsData.json";
 export default {
   name: "FilterRouteTab",
   components: {
-    ButtonGroupMultiButtons
+    ButtonGroupMultiButtons,
   },
   setup() {
     const store = useStore();
     const route = useRoute();
+    const defaultCity = ref("San Francisco");
+    const defaultStateCode = ref("CA");
     const searchPayload = ref({});
-    const activeRouteTemplate = ref("");
     const propertyRouteTemplate = ref([...settingsData.routeNames]);
+    const storeData = computed(() => store.getters.getStore);
 
-    const getIsActiveRouteTab = computed(() => {
-      return store.getters.getIsActiveRouteTab;
-    });
-    // const getSearchedData = computed(() => {
-    //   return store.getters.getSearchedData;
-    // });
-    watchEffect(() => {
-      switch (getIsActiveRouteTab.value) {
-        case "SalesNearMe":
-          store.commit("setActiveRoutePath", propertyRouteTemplate.value[0].id);
-          activeRouteTemplate.value = propertyRouteTemplate.value[0].id;
-          break;
-        case "RentsNearMe":
-          store.commit("setActiveRoutePath", propertyRouteTemplate.value[1].id);
-          activeRouteTemplate.value = propertyRouteTemplate.value[1].id;
-          break;
-        default:
-          store.commit("setActiveRoutePath", getIsActiveRouteTab.value);
-          activeRouteTemplate.value = getIsActiveRouteTab.value;
-          break;
-      }
-    });
-
-    function setPropertyListingTemplate(e){
+    function setPropertyListingTemplate(e) {
       let routeName = e.target.id;
-      let isActiveRoute = getIsActiveRouteTab.value;
-      let cityMayContainUnderscore = `${route.params.city}`;
-      let cityParam = cityMayContainUnderscore.replace(/_/g," ");
-      
-      if (routeName == isActiveRoute){
+      if (routeName == storeData.value.activeRoutePath) {
         return;
-      };
+      }
+      store.commit("setIsLoading", true);
+
+      let cityMayContainUnderscore = `${route.params.city}`;
+      let cityParam = cityMayContainUnderscore.replace(/_/g, " ");
+
       searchPayload.value = {
-        city: !route.params.city ? "San Francisco" : cityParam,
-        state_code: !route.params.slug ? "CA" : route.params.slug,
+        city: !route.params.city ? defaultCity.value : cityParam,
+        state_code: !route.params.slug
+          ? defaultStateCode.value
+          : route.params.slug,
         activeRouteTab: routeName,
       };
-      store.commit("setUseRouterPush", searchPayload.value);
+      store.commit("setActiveRoutePath", routeName);
       store.commit("setSearchedData", searchPayload.value);
-    };
-    return {
-      activeRouteTemplate,
-      propertyRouteTemplate,
-      setPropertyListingTemplate
+      store.commit("setUseRouterPush", searchPayload.value);
     }
- },
-}
+    return {
+      storeData,
+      defaultCity,
+      defaultStateCode,
+      propertyRouteTemplate,
+      setPropertyListingTemplate,
+    };
+  },
+};
 </script>
