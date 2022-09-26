601bc6b76daa7a229628b57206d425022033715f
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-09-26 10:17:05 +0100

    Code clean up and prettier fixes

--- src/components/buttonui/SearchInput.vue
+++ src/components/buttonui/SearchInput.vue
@@ -1,98 +1,112 @@
 <template>
-  <form 
+  <form
     role="search"
-    aria-autocomplete="list" 
+    aria-autocomplete="list"
     aria-labelledby="search-label"
-    autocomplete="off" 
-    v-on:submit.prevent="onSubmit">
-    <input  
+    autocomplete="off"
+    v-on:submit.prevent="onSubmit"
+  >
+    <input
       v-model="searchData"
       v-bind:class="{
         'rounded-b-lg focus:shadow-md ': !searchFilterIsActive,
       }"
       class="
-        w-full 
-        text-gray-700 
+        w-full
+        text-gray-700
         placeholder-gray-500
         rounded-t-lg
-        focus:outline-none 
+        focus:outline-none
         overflow-truncate
         focus:ring-teal-light
         focus:ring
-        focus:ring-offset-1 
+        focus:ring-offset-1
         focus:ring-offset-white
         pl-4
-        py-1 
-        pr-20"
-      v-bind="$attrs" 
-      type="search" 
+        py-1
+        pr-20
+      "
+      v-bind="$attrs"
+      type="search"
       aria-label="Search for State, City, Postal Code"
-      aria-controls="places-search-list" 
+      aria-controls="places-search-list"
       placeholder="Search for State, District/City, Zip, Neigbourhood/Area"
     />
     <button
       v-bind:class="{
-        'rounded-br-lg': !searchFilterIsActive}"
+        'rounded-br-lg': !searchFilterIsActive,
+      }"
       class="
-        bg-red-600 
-        hover:bg-red-800 
-        absolute 
-        right-0 
+        bg-red-600
+        hover:bg-red-800
+        absolute
+        right-0
         top-0
         w-14
         rounded-tr-lg
-        cursor-pointer"
-      v-bind="$attrs" 
-      role="button">
+        cursor-pointer
+      "
+      v-bind="$attrs"
+      role="button"
+    >
       <search-icon />
     </button>
   </form>
-  <div 
+  <div
     v-show="searchActive"
     v-bind:class="{
-      'relative border-t-0 border text-gray-600 w-full max-h-40 overflow-auto bg-white z-20 rounded-b-lg': 
-        searchFilterIsActive }">
-    <ul 
-      role="listbox" 
-      aria-labelledby="search-label" 
-      id="places-search-list" 
-      v-on:keydown="nextItem">
-      <span 
+      'relative border-t-0 border text-gray-600 w-full max-h-40 overflow-auto bg-white z-20 rounded-b-lg':
+        searchFilterIsActive,
+    }"
+  >
+    <ul
+      role="listbox"
+      aria-labelledby="search-label"
+      id="places-search-list"
+      v-on:keydown="nextItem"
+    >
+      <span
         v-bind:class="{
-          'bg-gray-100 block pl-4 py-3 text-xs font-medium': 
-            searchFilterIsActive 
-        }" disabled> 
+          'bg-gray-100 block pl-4 py-3 text-xs font-medium':
+            searchFilterIsActive,
+        }"
+        disabled
+      >
         {{ placeListTag }}
       </span>
-      <li 
-        v-for="(home, index) in searchPreferences" 
+      <li
+        v-for="(home, index) in searchPreferences"
         :key="index"
         :data-suggestion-item="index"
         :title="`${home.city}, ${home.state_code}`"
         :id="`${home.city}`"
         :nonce="`${home.state_code}`"
         role="option"
-        v-on:click="submitClicked" 
+        v-on:click="submitClicked"
         aria-selected="false"
         class="
-          text-gray-600 
-          border-b 
-          font-normal 
-          text-sm 
-          hover:bg-gray-100 
-          py-1 
-          px-3" 
+          text-gray-600
+          border-b
+          font-normal
+          text-sm
+          hover:bg-gray-100
+          py-1
+          px-3
+        "
         v-bind:class="{
-          'bg-gray-100 focus:bg-gray-200': currentDataIndex == index}">
+          'bg-gray-100 focus:bg-gray-200': currentDataIndex == index,
+        }"
+      >
         <div class="flex space-x-2">
           <span>
-            <location-icon class="mt-1 text-gray-600 text-xs"/>
+            <location-icon class="mt-1 text-gray-600 text-xs" />
           </span>
-          <span 
-            :id="`${home.city}`" 
-            :nonce="`${home.state_code}`" 
-            class="w-full">
-            {{ home.city }}, {{ home.state_code}}
+          <span
+            :id="`${home.city}`"
+            :nonce="`${home.state_code}`"
+            class="w-full"
+          >
+            {{ home.city }}, {{ home.state_code }}
             <legend class="text-xs font-normal">City</legend>
           </span>
         </div>
@@ -101,16 +115,9 @@
   </div>
 </template>
 <script>
-import { 
-  ref, 
-  reactive, 
-  onMounted, 
-  watch, 
-  computed, 
-  onBeforeUnmount, 
-  watchEffect
-} from "vue";
+import { ref, reactive, onMounted, watch, computed, onUnmounted } from "vue";
 import { useStore } from "vuex";
+import { useRoute } from "vue-router";
 import jsonProperties from "@/api/autoComplete.json";
 import SearchIcon from "@/assets/icons/SearchIcon.vue";
 import LocationIcon from "@/assets/icons/LocationIcon.vue";
@@ -127,6 +134,8 @@ export default {
     const activeRouteTab = ref("");
     const searchPayload = ref({});
     const store = useStore();
+    const route = useRoute();
+    // const router = useRouter();
     const searchActive = ref(false);
     const currentDataIndex = ref(-1);
 
@@ -134,8 +143,10 @@ export default {
     const removeDuplicateHomeData = autoComplete.allProperties.reduce(
       (accumulator, currentValue) => {
         let duplicateScreening = !accumulator.some(
-          (obj) => obj.city === currentValue.city && obj.state === currentValue.state)
-        if (duplicateScreening) { 
+          (obj) =>
+            obj.city === currentValue.city && obj.state === currentValue.state
+        );
+        if (duplicateScreening) {
           accumulator.push({
             city: currentValue.city,
             state: currentValue.state,
@@ -145,131 +156,110 @@ export default {
           });
         }
         return accumulator;
-      },[]);
+      },
+      []
+    );
     const searchPreferences = computed(() => {
       if (!searchActive.value) return "";
       return removeDuplicateHomeData.filter((data) => {
-        let increaseSearchFilterField = 
+        let increaseSearchFilterField =
           data.state + data.city + data.state_code;
         let searched = increaseSearchFilterField
           .toLowerCase()
           .includes(searchData.value.toLowerCase());
         return searched;
-      })
-    })
+      });
+    });
     const searchFilterIsActive = computed(() => {
       return !searchActive.value
-        ? false 
-        : searchPreferences.value.length > 0 
-        ? true 
+        ? false
+        : searchPreferences.value.length > 0
+        ? true
         : false;
-    })
-    const placeListTag = computed(() =>{
-      return searchPreferences.value.length > 0 ? "Places" : ""; 
-    })
+    });
+    const placeListTag = computed(() => {
+      return searchPreferences.value.length > 0 ? "Places" : "";
+    });
 
     watch(searchData, () => {
       let searchedWordIsTwo = searchData.value.length >= 2 ? true : false;
       searchActive.value = searchedWordIsTwo;
     });
 
-    const getIsActiveRouteTab = computed(() => {
-      return store.getters.getIsActiveRouteTab;
-    });
-
-    watchEffect(() => {
-
-      switch (getIsActiveRouteTab.value) {
-        case "RentPage":
-        case "list-for-rent":
-          activeRouteTab.value = "list-for-rent"
-          break;
-        case "SoldPage": 
-        case "list-sold":
-          activeRouteTab.value = "list-sold"
-          break;
-        default:
-          activeRouteTab.value = "list-for-sale"
-          break;
-      }
-    });
-
     async function onSubmit(e) {
       if (!searchData.value) {
         return;
-      };
+      }
 
       if (!searchFilterIsActive.value && searchData.value) {
         let defaultSearch = {
           city: "San Francisco",
           state_code: "CA",
-          activeRouteTab: activeRouteTab.value
-        }
-        // await store.dispatch("setPropertiesFromRemoteApi", defaultSearch);
+          activeRouteTab: route.name,
+        };
         store.commit("setUseRouterPush", defaultSearch);
         store.commit("setSearchedData", defaultSearch);
-        // console.log("ActiveRouteTab:", defaultSearch);
-      }; 
-      if (searchFilterIsActive.value && currentDataIndex.value <= 0){
-        searchPayload.value = {
-          city: e.target.nextElementSibling.children["places-search-list"].children[1].id,
-          state_code: 
-            e.target.nextElementSibling.children["places-search-list"].children[1].nonce,
-          activeRouteTab: activeRouteTab.value
-        }
-        // await store.dispatch("setPropertiesFromRemoteApi", searchPayload.value);
-        store.commit("setUseRouterPush", searchPayload.value);
-        store.commit("setSearchedData", searchPayload.value);
-      };
-      if (searchFilterIsActive.value && currentDataIndex.value > 0){
+      }
+      if (searchFilterIsActive.value && currentDataIndex.value <= 0) {
         searchPayload.value = {
           city: e.target.nextElementSibling.children["places-search-list"]
-            .children[currentDataIndex.value+1].id,
-          state_code: 
-            e.target.nextElementSibling.children["places-search-list"].children[currentDataIndex.value+1].nonce,
-          activeRouteTab: activeRouteTab.value
+            .children[1].id,
+          state_code:
+            e.target.nextElementSibling.children["places-search-list"]
+              .children[1].nonce,
+          activeRouteTab: route.name,
         };
-
-        // await store.dispatch("setPropertiesFromRemoteApi", searchPayload.value);
         store.commit("setUseRouterPush", searchPayload.value);
         store.commit("setSearchedData", searchPayload.value);
-
       }
+      if (searchFilterIsActive.value && currentDataIndex.value > 0) {
+        searchPayload.value = {
+          city: e.target.nextElementSibling.children["places-search-list"]
+            .children[currentDataIndex.value + 1].id,
+          state_code:
+            e.target.nextElementSibling.children["places-search-list"].children[
+              currentDataIndex.value + 1
+            ].nonce,
+          activeRouteTab: route.name,
+        };
 
-    };
-    async function submitClicked(e){
+        store.commit("setUseRouterPush", searchPayload.value);
+        store.commit("setSearchedData", searchPayload.value);
+      }
+    }
+    async function submitClicked(e) {
       searchPayload.value = {
         city: e.target.id,
         state_code: e.target.nonce,
-        activeRouteTab: activeRouteTab.value
-      }
+        activeRouteTab: route.name,
+      };
       store.commit("setUseRouterPush", searchPayload.value);
-      // await store.dispatch("setPropertiesFromRemoteApi", searchPayload.value);
       store.commit("setSearchedData", searchPayload.value);
-
-    };
+    }
     onMounted(() => {
       document.addEventListener("keydown", nextItem);
     });
 
-    onBeforeUnmount(() => {
+    onUnmounted(() => {
       document.removeEventListener("keydown", nextItem);
     });
 
-    function nextItem(e){
+    function nextItem(e) {
       if (
-        searchFilterIsActive.value && 
-        e.keyCode == 40 && 
-        currentDataIndex.value < searchPreferences.value.length -1){
+        searchFilterIsActive.value &&
+        e.keyCode == 40 &&
+        currentDataIndex.value < searchPreferences.value.length - 1
+      ) {
         currentDataIndex.value++;
-      } else if ( 
-        searchFilterIsActive.value && 
-        e.keyCode == 38 && 
-        currentDataIndex.value > -1){
+      } else if (
+        searchFilterIsActive.value &&
+        e.keyCode == 38 &&
+        currentDataIndex.value > -1
+      ) {
         currentDataIndex.value--;
       }
-    };
-    return{
+    }
+    return {
       autoComplete,
       searchData,
       activeRouteTab,
@@ -282,8 +272,8 @@ export default {
       onSubmit,
       submitClicked,
       currentDataIndex,
-      nextItem
-    }
+      nextItem,
+    };
   },
-}
+};
 </script>
