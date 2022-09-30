1d0f86667665ecc99de17af5170b97398b564a0b
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-09-30 12:31:44 +0100

    Feature: Retain app state and config after page refresh by the user

--- src/components/filter/FilterButtonPets.vue
+++ src/components/filter/FilterButtonPets.vue
@@ -1,71 +1,92 @@
 <template>
   <div>
-    <dropdown-button 
+    <dropdown-button
       v-show="isRentPage"
-      v-bind:filterActivated="buttonActivated" 
-      v-bind:buttonTitle="petsTitle" 
-      menuTitle="Pets">
-      <button-radio v-model:value="isDogsAllowed">Dogs Allowed</button-radio>
-      <button-radio v-model:value="isCatsAllowed">Cats Allowed</button-radio>
-    </dropdown-button> 
+      v-bind:filterActivated="buttonActivated"
+      v-bind:buttonTitle="petsTitle"
+      menuTitle="Pets"
+    >
+      <button-radio @click="dogsHandler" v-model:value="isDogsAllowed"
+        >Dogs Allowed</button-radio
+      >
+      <button-radio @click="catsHandler" v-model:value="isCatsAllowed"
+        >Cats Allowed</button-radio
+      >
+    </dropdown-button>
   </div>
 </template>
 <script>
-import { ref, watchEffect, computed } from "vue";
+import { ref, watchEffect, computed, onBeforeMount } from "vue";
 import { useStore } from "vuex";
+import { useRoute } from "vue-router";
 import { DropdownButton, ButtonRadio } from "@/components/buttonui";
 import settingsData from "@/api/settingsData.json";
 export default {
   name: "FilterButtonPets",
   components: {
     DropdownButton,
-    ButtonRadio
+    ButtonRadio,
   },
   setup() {
     const store = useStore();
-    const isDogsAllowed = ref(false);
-    const isCatsAllowed = ref(false);
+    const route = useRoute();
+    const storeData = computed(() => store.getters.getStore);
+    const isDogsAllowed = ref("");
+    const isCatsAllowed = ref("");
     const isRentPage = ref(Boolean);
     const routeNames = [...settingsData.routeNames];
-    // const dogTitle = ref("");
-    // const catTitle = ref("");
-
-    const getActiveRoutePath = computed(
-      () => store.getters.getIsActiveRouteTab);
 
     const petsTitle = computed(() => {
-      return !isDogsAllowed.value && !isCatsAllowed.value 
+      return !storeData.value.propertyFilters.allowsDogs &&
+        !storeData.value.propertyFilters.allowsCats
         ? "Pets"
-        : isDogsAllowed.value && isCatsAllowed.value
-        ? "Cats & Dogs"
-        : isDogsAllowed.value
-        ? "Dogs Only"
-        : "Cats Only" 
+        : storeData.value.propertyFilters.allowsDogs &&
+          storeData.value.propertyFilters.allowsCats
+        ? "Pets (2)"
+        : storeData.value.propertyFilters.allowsDogs
+        ? "Dog Pets"
+        : "Cat Pets";
     });
-    const buttonActivated = computed(() => 
-      petsTitle.value == "Pets" ? false : true);
+    const buttonActivated = computed(() =>
+      petsTitle.value == "Pets" ? false : true
+    );
 
     watchEffect(() => {
-      isRentPage.value = 
-        getActiveRoutePath.value == routeNames[1].id ? true : false;
+      isRentPage.value =
+        storeData.value.activeRoutePath == routeNames[1].id ? true : false;
     });
-
     watchEffect(() => {
-      let dogsAreAllowed = isDogsAllowed.value ? isDogsAllowed.value : "";
+      isCatsAllowed.value = storeData.value.propertyFilters.allowsCats;
+      isDogsAllowed.value = storeData.value.propertyFilters.allowsDogs;
+    });
+    function dogsHandler() {
+      let dogsAreAllowed = isDogsAllowed.value ? "" : true;
       store.commit("setIsDogsAllowed", dogsAreAllowed);
-    });
-    watchEffect(() => {
-      let catsAreAllowed = isCatsAllowed.value ? isCatsAllowed.value : "";
+    }
+    function catsHandler() {
+      let catsAreAllowed = isCatsAllowed.value ? "" : true;
       store.commit("setIsCatsAllowed", catsAreAllowed);
+    }
+    onBeforeMount(() => {
+      if (route.query.dogsAllowed) {
+        isDogsAllowed.value = route.query.dogsAllowed;
+        store.commit("setIsDogsAllowed", isDogsAllowed.value);
+      }
+      if (route.query.catsAllowed) {
+        isCatsAllowed.value = route.query.catsAllowed;
+        store.commit("setIsCatsAllowed", isCatsAllowed.value);
+      }
+      console.log(">> onBeforeMount PETS ALLOWED");
     });
-
     return {
       petsTitle,
       buttonActivated,
       isDogsAllowed,
       isCatsAllowed,
-      isRentPage
-    }
-  }
-}
+      isRentPage,
+      dogsHandler,
+      catsHandler,
+    };
+  },
+};
 </script>
