079cf277078dbd65d9f638af08da9e7cff03fcca
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-10-13 11:18:20 +0100

    Feature: Highly dynamic contact form in real time, that grabs all the days in a week range and also the custom working hours associated with each day

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,6 @@
+export { default as InformationIcon } from "@/assets/icons/InformationIcon.vue";
+export { default as CloseMobileMenu } from "@/assets/icons/CloseMobileMenu.vue";
+export { default as FilterIcon } from "@/assets/icons/FilterIcon.vue";
 export { default as ChevronDown } from "@/assets/icons/ChevronDown.vue";
 export { default as ShareIcon } from "@/assets/icons/ShareIcon.vue";
 export { default as BedIcon } from "@/assets/icons/BedIcon.vue";
@@ -8,6 +11,7 @@ export { default as SquareFeetIcon } from "@/assets/icons/SquareFeetIcon.vue";
 export { default as ImageCountIcon } from "@/assets/icons/ImageCountIcon.vue";
 export { default as ArrowBackIcon } from "@/assets/icons/ArrowBackIcon.vue";
 export { default as ChevronRight } from "@/assets/icons/ChevronRight.vue";
+export { default as ChevronLeft } from "@/assets/icons/ChevronLeft.vue";
 export { default as DollarIcon } from "@/assets/icons/DollarIcon.vue";
 export { default as ParkingIcon } from "@/assets/icons/ParkingIcon.vue";
 export { default as OutdoorIcon } from "@/assets/icons/OutdoorIcon.vue";
