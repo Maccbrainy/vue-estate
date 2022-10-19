6f0a087be0feecfc6b19bea7129a24024e7cf139
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-10-19 07:40:12 +0100

    Code refactor

--- src/components/WidgetImageSlider.vue
+++ src/components/WidgetImageSlider.vue
@@ -1,26 +1,38 @@
 <template>
   <div
-    v-bind:class="{ 'animate-pulse': !propertyListingPhotos}" 
-    class="relative w-full h-44 bg-gray-200 rounded-xl overflow-hidden">
+    v-bind:class="{
+      'animate-pulse':
+        isDefaultImage || storeData.fetchingIsBusy || storeData.isLoading,
+    }"
+    class="relative w-full h-44 bg-gray-200 rounded-xl overflow-hidden"
+  >
     <img
-      v-bind:class="{ 'hidden': !propertyListingPhotos }" 
-      v-bind:src="listingPhotos" 
-      alt="" 
-      class="w-full h-full object-center object-cover"/>
+      v-show="
+        (!isDefaultImage && !storeData.isLoading) ||
+        (!isDefaultImage && !storeData.fetchingIsBusy)
+      "
+      v-bind:src="listingPhotos"
+      alt=""
+      class="w-full h-full object-center object-cover"
+    />
   </div>
 </template>
 <script>
 import { computed } from "vue";
+import { useStore } from "vuex";
 export default {
   name: "WidgetImageSlider",
   props: ["listingPhotos"],
-  setup(props){
-    const propertyListingPhotos = computed(() => 
-      props.listingPhotos == "defaultImage" ? false : true
+  setup(props) {
+    const store = useStore();
+    const storeData = computed(() => store.getters.getStore);
+    const isDefaultImage = computed(() =>
+      props.listingPhotos == "defaultImage" ? true : false
     );
     return {
-      propertyListingPhotos
-    }
-  }
-}
+      isDefaultImage,
+      storeData,
+    };
+  },
+};
 </script>
