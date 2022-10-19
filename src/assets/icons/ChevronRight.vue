74160cb183135dea65f221e7c8ff642c2015ec1a
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-10-19 07:27:55 +0100

    css styling update

--- src/assets/icons/ChevronRight.vue
+++ src/assets/icons/ChevronRight.vue
@@ -1,7 +1,7 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    class="h-4 w-4 m-auto"
+    class="h-6 w-6 m-auto"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
