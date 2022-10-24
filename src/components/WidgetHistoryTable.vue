ae907492f735de8fe1536a943973e6d14dc35073
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-10-24 18:45:59 +0100

    Mobile responsive design

--- src/components/WidgetHistoryTable.vue
+++ src/components/WidgetHistoryTable.vue
@@ -15,7 +15,9 @@
         rounded-tl-lg rounded-tr-lg rounded-br-lg rounded-bl-lg
       "
     >
-      <thead>
+      <thead
+        v-bind:class="{ 'sf:hidden': propertyTableHeaderTitle.length > 3 }"
+      >
         <tr class="w-full py-3 text-gray-600">
           <th
             v-for="title in propertyTableHeaderTitle"
@@ -39,14 +41,48 @@
           :key="propertyHistory.date || propertyHistory.assessment"
           class="border w-full"
         >
-          <td v-if="propertyHistory.date" class="w-auto py-2 px-4">
+          <td v-if="propertyTableHeaderTitle.length > 3"
+            class="w-auto py-2 px-3 md:hidden"
+            colspan="1"
+          >
+            <div class="flex flex-row">
+              <div class="w-9/12 flex flex-col sm:px-4">
+                <span
+                  v-if="propertyHistory.date"
+                  class="w-auto font-semibold text-gray-700"
+                >
+                  {{ format(new Date(propertyHistory.date), " d/M/yyy")}}
+                </span>
+                <span
+                  v-if="propertyHistory.event_name"
+                  class="w-auto font-semibold text-gray-700"
+                >
+                  {{ propertyHistory.event_name }}
+                </span>
+                <span class="w-auto">{{ propertyHistory.source }}</span>
+              </div>
+              <div class="w-3/12 flex flex-col pr-4">
+                <span
+                  v-if="propertyHistory.price"
+                  class="w-auto font-semibold text-gray-700"
+                >
+                  {{ `Price: $${addCommaToNumberFormat(propertyHistory.price)}` }}
+                </span>
+                <span class="w-auto xs:hidden">
+                  {{ `Price/Sqft: $${addCommaToNumberFormat(propertyHistory.sqft)}` }}
+                </span>
+              </div>
+            </div>
+          </td>
+
+          <td v-if="propertyHistory.date" class="w-auto py-2 px-4 sf:hidden">
             {{ format(new Date(propertyHistory.date), " d/M/yyy")}}
           </td>
           <td v-else class="w-auto py-2 px-4">
             {{ propertyHistory.year }}
           </td>
 
-          <td v-if="propertyHistory.price" class="w-auto py-2 px-4">
+          <td v-if="propertyHistory.price" class="w-auto py-2 px-4 sf:hidden">
             ${{ addCommaToNumberFormat(propertyHistory.price) }}
           </td>
           <td v-else-if="propertyHistory.assessment" class="w-auto py-2 px-4">
@@ -57,7 +93,7 @@
           </td>
 
 
-          <td v-if="propertyHistory.event_name" class="w-auto py-2 px-4">
+          <td v-if="propertyHistory.event_name" class="w-auto py-2 px-4 sf:hidden">
             {{ propertyHistory.event_name }}
           </td>
           <td v-if="propertyHistory.tax" class="w-auto py-2 px-4">
@@ -65,18 +101,18 @@
           </td>
 
 
-          <td class="w-auto py-2 px-4">
+          <td class="w-auto py-2 px-4 sf:hidden">
             ${{ addCommaToNumberFormat(propertyHistory.sqft) }}
           </td>
 
 
-          <td class="w-auto py-2 px-4 flex justify-between">
+          <td class="w-auto py-2 px-4 flex justify-between sf:hidden">
             <span>{{
               propertyHistory.datasource_name
                 ? `${propertyHistory.source} | ${propertyHistory.datasource_name}`
                 : propertyHistory.source
             }}</span>
-            <button
+            <button class="sf:hidden"
               v-show="propertyHistory.event_name == `Sold`"
               v-on:click="openRecord = !openRecord"
             >
@@ -96,7 +132,7 @@
         </tr>
         <tr
           v-show="openRecord"
-          class="w-full bg-gray-100 py-3 text-gray-600 border"
+          class="w-full bg-gray-100 py-3 text-gray-600 border sf:hidden"
         >
           <td class="w-auto py-2 px-3" colspan="5">
             <div class="flex flex-wrap">
@@ -159,24 +195,25 @@
     </table>
     <div v-else class="text-gray-400 text-lg -mt-3">No {{ propertyTitle }} for this property</div>
     <span
-      v-show="propertyContentData.length > 4 && !toggleTable"
+      v-show="propertyContentData.length > 2 && !toggleTable"
       class="bg-gradient-to-t from-white h-10 w-full -mt-12"
     >
     </span>
     <button
-      v-show="propertyContentData.length > 4"
+      v-show="propertyContentData.length > 2"
       v-on:click="toggleTable = !toggleTable"
       class="
         w-28
         mt-6
         px-4
         py-2
-        text-gray-600
-        font-bold
+        text-gray-500
+        font-semibold
         text-base
-        border border-gray-400
+        border
         rounded-lg
         bg-white
+        hover:border-gray-700
         active:border-gray-800
       "
     >
@@ -214,12 +251,15 @@ export default {
 
     const propertyHistoriesToShowAtDefault = computed(() => {
       let historyData = "";
-      if (props.propertyContentData.length > 4) {
+      if (props.propertyContentData.length > 2) {
         historyData = props.propertyContentData.filter((data, index) =>
-          index < 4 ? data : ""
+          index < 2 ? data : ""
         );
       }
-      if (props.propertyContentData.length > 0 && props.propertyContentData.length <= 4) {
+      if (
+        props.propertyContentData.length > 0 &&
+        props.propertyContentData.length <= 2
+      ) {
         historyData = props.propertyContentData;
       }
       return historyData;
