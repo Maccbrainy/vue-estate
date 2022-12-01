5d98ea61fb461a15bfad9d21d6ccd812f9e749b7
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-01 10:09:08 +0100

    Files removed;file structure clean up

--- src/composables/searchInfoQuerySanitizer.js
+++
@@ -1,9 +0,0 @@
-export default function searchInfoQuerySanitizer(propSlug) {
-  let searchInfo = propSlug;
-  if (!searchInfo) return;
-  let validatedSearchInfo =
-    searchInfo.charAt(0).toUpperCase() + searchInfo.substring(1).toLowerCase();
-  return {
-    validatedSearchInfo,
-  };
-}
