f64db01ef31fc51b81e5a4005ff9c9c4463ef6e8
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-01 10:14:52 +0100

    Initial commit folder file clean up

---
+++ src/layouts/index.js
@@ -0,0 +1,2 @@
+export { default as BaseLayout } from "../layouts/BaseLayout.vue";
+export { default as HomePageLayout } from "../layouts/HomePageLayout.vue";
---
+++ src/views/index.js
@@ -0,0 +1,4 @@
+export { default as TheHomePage } from "../views/TheHomePage.vue";
+export { default as ThePropertyListPage } from "../views/ThePropertyListPage.vue";
+export { default as ThePropertyDetailedPage } from "../views/ThePropertyDetailedPage.vue";
+export { default as TheMortgagePage } from "../views/TheMortgagePage.vue";
