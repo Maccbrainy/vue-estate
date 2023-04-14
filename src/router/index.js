62c9ed58775c09594dcd3de704b8d9d334f75838
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-04-14 20:02:28 +0100

    Always scroll to top on route path changes

--- src/router/index.js
+++ src/router/index.js
@@ -236,6 +236,12 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior(){
+    //always scroll to top
+    return {
+      top: 0
+    }
+  }
 });
 
 router.afterEach((to) => {
